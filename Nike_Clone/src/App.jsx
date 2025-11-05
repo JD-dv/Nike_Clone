37ca547981fa6c139755e52e848a0ae09324474e
J_Dejolde <dejoldedemosthenes2003@gmail.com>
2025-11-05 22:49:26 +0800

    Add grid layout and improve overall layout

--- Nike_Clone/src/App.jsx
+++ Nike_Clone/src/App.jsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home.jsx"
-import Test from "./pages/Test.jsx"
+
 
 function App() {
   return (
   <Routes>
    <Route path="/" element={<Home />}/>
-   <Route path="/test" element={<Test />}/>
   </Routes>
   )
 }
--- Nike_Clone/src/pages/Home.jsx
+++ Nike_Clone/src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import { useState, useEffect } from "react";
 import Home_page from "../assets/images/home_page.png";
 import Nike_1 from "../assets/images/Nike_1.svg";
-import nike_shoe_1 from "../assets/images/nike_shoe_1.svg"
-import nike_shoe_2 from "../assets/images/nike_shoe_2.svg"
-import nike_shoe_3 from "../assets/images/nike_shoe_3.svg"
+import nike_shoe_1 from "../assets/images/nike_shoe_1.svg";
+import nike_shoe_2 from "../assets/images/nike_shoe_2.svg";
+import nike_shoe_3 from "../assets/images/nike_shoe_3.svg";
+import shoe_grid from "../assets/images/shoe_grid.png";
+import blue_grid from "../assets/images/blue_grid.png";
+import long_grid from "../assets/images/long_grid.png";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 
@@ -23,7 +27,7 @@ function Home() {
             <Navbar />
           </nav>
           <h1
-            className="absolute top-[250px] left-10 font-inter  text-[45px] text-shadow-lg/10  bg-gradient-to-l from-[#a7adee] via-[#cfdbf0cc] to-white bg-clip-text text-transparent
+            className="absolute top-[250px] left-10 font-inter  text-[45px] text-shadow-lg/10  bg-gradient-to-l from-[#b7bbe4] via-[#6f9debcc] to-white bg-clip-text text-transparent
           sm:text-[60px] md:text-[75px] lg:text-[80px] "
           >
             Where Simplicity Meets Comfort
@@ -36,12 +40,51 @@ function Home() {
         </div>
       </main>
 
-      <section className="mt-50 w-screen">
-        <h1 className="luckyguy flex justify-center text-9xl max-sm:text-7xl">Life Style</h1>
-        <div className="flex relative w-screen">
-          <img src={nike_shoe_1} alt=""  className="absolute "/>
-          <img src={nike_shoe_2} alt=""  className="absolute "/>
-          <img src={nike_shoe_3} alt=""  className="absolute "/>
+      <section className="mt-80 w-screen sm:h-[1800px] md:h-[900px] lg:h-[950px]">
+        <h1 className="luckyguy flex justify-center text-9xl max-sm:text-7xl ">
+          Life Style
+        </h1>
+        <div className="flex max-sm:flex-col sm:flex-col relative w-screen lg:mt-10">
+          <img
+            src={nike_shoe_1}
+            alt=""
+            className="md:absolute md:w-[320px] lg:w-[520px]"
+          />
+          <img
+            src={nike_shoe_2}
+            alt=""
+            className="md:absolute md:w-[320px] lg:w-[520px] md:top-[190px]"
+            style={{ left: "32vw" }}
+          />
+          <img
+            src={nike_shoe_3}
+            alt=""
+            className="md:absolute md:w-[320px] md:right-0 lg:w-[520px]"
+          />
+        </div>
+      </section>
+
+      <section className="h-[1000px] mx-5 max-sm:mt-80">
+        <h1 className="font-inter flex justify-center text-3xl sm:text-5xl lg:text-7xl">
+          Comfort Style Versatility.
+        </h1>
+        <div className="flex justify-center gap-10 mt-10 max-sm:flex-col">
+          <div className="">
+            <img src={shoe_grid} alt="" className="mb-8" />
+            <img src={blue_grid} alt="" />
+          </div>
+          <div>
+            <img src={long_grid} alt="" />
+          </div>
+        </div>
+
+        <div className="mt-10 ml-20">
+          <h2 className="mb-5 text-4xl sm:mb-10 sm:text-5xl">Your perfect everyday Nike.</h2>
+          <Link>
+            <span className="bg-[#38506b] max-sm:text-2xl  p-2 rounded-lg text-[#e6ebf0d3] sm:text-[25px]">
+              Shop Life Style Shoes
+            </span>
+          </Link>
         </div>
       </section>
     </>
