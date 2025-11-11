73e732463aecc1e50164f0a864ed6e8442e5c67a
J_Dejolde <dejoldedemosthenes2003@gmail.com>
2025-11-11 22:08:56 +0800

    Implement responsive sidebar for desktop and mobile devices

--- Nike_Clone/src/components/Sidebar.jsx
+++ Nike_Clone/src/components/Sidebar.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
+import nike_1 from "../assets/images/nike_1.svg";
+import panel_left from "../assets/images/panel_left.svg";
+import { useState } from "react";
 
-function Sidebar() {
+function Sidebar({panel, mediumDev}) {
+
+
   return (
-    <aside className="flex flex-col  p-4 sticky top-0 rounded-2xl">
-      <h1 className="mb-25">Nike Store</h1>
+    <aside className={`flex flex-col  p-5 sticky top-0 ${panel ? 'hidden': ''} ${mediumDev ? 'max-md:block ':'max-md:hidden'} `}>
+      <div className="flex items-center gap-x-2 mb-16">
+        <img src={nike_1} alt="Nike Logo" className="w-[80px]" />
+      </div>
       <nav className="flex flex-col gap-y-2">
         <Link>Shoes</Link>
+        <Link>Apparel</Link>
       </nav>
     </aside>
   );
--- Nike_Clone/src/pages/Shop.jsx
+++ Nike_Clone/src/pages/Shop.jsx
@@ -1,17 +1,39 @@
 import Sidebar from "../components/Sidebar";
-
+import panel_left from "../assets/images/panel_left.svg";
+import panel_right from "../assets/images/panel_right.svg";
+import { useState } from "react";
 function Shop() {
+  const [panel, setpanel] = useState(false);
+  const [mediumDev, setmeddiumDev] = useState(false);
+
+  function handlePanel() {
+    setpanel((prev) => !prev);
+  }
+
+  
+    function handleMediumDevice(){
+      setmeddiumDev(prev => !prev)
+    }
+  
+
   return (
-    <div className="flex h-screen p-2 overflow-hidden bg-gray-200">
-      <Sidebar />
-      <main className="flex-1 bg-gray-200 p-2 overflow-y-auto">
+    <div className="flex items-start h-screen p-2 overflow-hidden bg-gray-200">
+      <Sidebar panel={panel} mediumDev={mediumDev}/>
+      <span className="mt-10 cursor-pointer" onClick={() =>{
+        handlePanel();
+        handleMediumDevice();
+      }}>
+        <img
+          src={panel ? panel_right : panel_left}
+          alt=""
+          className="opacity-60 w-[22px]"
+        />
+      </span>
+      <main className="flex-1 bg-gray-200 p-2 overflow-y-auto h-full">
         <div className="bg-white min-h-screen rounded-2xl p-2">
-            <div className="space-y-6">
-          <h2 className="text-2xl font-bold">Main Section</h2>
-          {[...Array(30)].map((_, i) => (
-            <p key={i}>This is line {i + 1} — scroll down to test.</p>
-          ))}
-        </div>
+          <div className="space-y-6 p-5">
+            <h2 className="text-2xl font-bold">Main Section</h2>
+          </div>
         </div>
       </main>
     </div>
